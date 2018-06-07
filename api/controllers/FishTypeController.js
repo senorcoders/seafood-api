44c9c30d72ee53e6700bfe1f9ca10940127be3bf
Osmany Jirón <osmany@senorcoders.com>
2018-06-07 15:26:53 -0600

    cuando se obtiene  mariscos se agrega el store y el owner of store

--- api/controllers/FishTypeController.js
+++ api/controllers/FishTypeController.js
@@ -7,6 +7,15 @@ module.exports = {
             let fisher = await FishType.findOne({ name: req.params.name })
             let fishers = await Fish.find({ type : fisher.id }).populate('type').populate("store").paginate({page: req.params.page, limit: req.params.limit});
 
+            fishers = await Promise.all(fishers.map(async function(m){
+                if(m.store === null)
+                    return m;
+                    
+                m.store.owner = await User.findOne({id: m.store.owner});
+
+                return m;
+            }));
+
             res.json(fishers);
         }
         catch(e){
