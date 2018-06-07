8c818e5aa736a7b7f63cdf2ea1588482dd2198d2
Osmany Jirón <osmany@senorcoders.com>
2018-06-07 10:12:06 -0600

    cuando se obtiene un carrito se envia con la informacion del maricos

--- api/controllers/ShoppingCartController.js
+++ api/controllers/ShoppingCartController.js
@@ -1,28 +1,50 @@
 
 module.exports = {
-  addItem: async (req, res)=>{
-    try{
-        let id = req.param("id"), 
-        item = {
-            shoppingCart: id,
-            fish: req.param("fish"),
-            quantity: req.param("quantity"),
-            price: req.param("price")
-        };
+    addItem: async (req, res) => {
+        try {
+            let id = req.param("id"),
+                item = {
+                    shoppingCart: id,
+                    fish: req.param("fish"),
+                    quantity: req.param("quantity"),
+                    price: req.param("price")
+                };
 
-        let itemShopping = await ItemShopping.create(item);
-        let cart = await ShoppingCart.findOne({ id }).populate("items");
-        cart.items = await Promise.all(cart.items.map(async (i)=>{
-            i.fish = await Fish.findOne({ id: i.fish });
-            return i;
-        }));
-        res.json(cart);
+            let itemShopping = await ItemShopping.create(item);
+            let cart = await ShoppingCart.findOne({ id }).populate("items");
+            cart.items = await Promise.all(cart.items.map(async (i) => {
+                i.fish = await Fish.findOne({ id: i.fish });
+                return i;
+            }));
+            res.json(cart);
+        }
+        catch (e) {
+            console.error(e);
+            res.serverError(e);
+        }
+    },
+
+    getPopulateXID: async function (req, res) {
+        try {
+
+            let id = req.param("id");
+            let cart = await ShoppingCart.findOne().where({ id }).populate("items");
+            if (cart === undefined) {
+                return res.status(400).send('not found');
+            }
+
+            cart.items = await Promise.all(cart.items.map(async function (it) {
+                it.fish = await Fish.findOne({ id: it.fish }).populate("type");
+                return it;
+            }));
+
+            res.json(cart);
+        }
+        catch (e) {
+            console.error(e);
+            res.serverError(e);
+        }
     }
-    catch(e){
-        console.error(e);
-        res.serverError(e);
-    }
-  }
 
 };
 
