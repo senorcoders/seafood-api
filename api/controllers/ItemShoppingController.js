e4571f3e37fead4dd7bb33a10505c800e29dfa37
Osmany Jirón <osmany@senorcoders.com>
2018-07-05 10:01:20 -0600

    compuse el endpoint para obtener los productos pagados.

--- api/controllers/ItemShoppingController.js
+++ api/controllers/ItemShoppingController.js
@@ -95,6 +95,7 @@ module.exports = {
 
             //filtrmos los items que ya esten pagados
             itemsShoppings = itemsShoppings.filter(function(it){
+                if( it.shoppingCart === null || it.shoppingCart.status === null ) return false;
                 return it.shoppingCart.status === "paid" && it.shippingStatus === "shipped";
             });
 
