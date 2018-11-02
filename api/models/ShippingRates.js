e04419f3b5e5109b058ffbfdc6d074203b203a8b
Milton Espinoza <milton@senorcoders.com>
2018-11-02 11:38:30 -0600

    adding models for shipping rates

---
+++ api/models/ShippingRates.js
@@ -0,0 +1,51 @@
+/**
+ * ShippingRates.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    weight : {
+      type: "number",
+      required: true
+    },
+    type: {
+      type: "string",
+      description: "pounds or kg",
+      required: true
+    },
+    cost : {
+      type: "number",
+      required: true
+    },
+    operation : {
+      type: "string",
+      description: "Under, Over",
+      defaultTo: "under"
+    },
+    sellerCountry : {
+      type: "string",
+      description: "country code"
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    
+    
+  },
+
+};
+
