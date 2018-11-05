370df5a384ee487e3f45699d2aea3a6ec1fa9a1b
Milton Espinoza <milton@senorcoders.com>
2018-11-05 15:38:44 -0600

    adding shipping calculator function

--- api/controllers/ShippingRatesController.js
+++ api/controllers/ShippingRatesController.js
@@ -6,7 +6,64 @@
  */
 
 module.exports = {
-  
 
+    getShippingRate: async (country, weight, type) => {
+        console.log( country ); 
+        console.log( weight ); 
+        console.log( type ); 
+        try {
+            shippingRate = 0;
+            shipping = await ShippingRates.find( { sellerCountry: country } )
+                .sort( [{ weight: 'ASC' }] )
+                .then( 
+                    result => {
+                        var BreakException = {};
+                        try {
+                            result.forEach( row => {
+                                let over=false;
+                                let overCost = 0;
+                                let under = false;
+                                let unerCost = 0;
+                                let shippingCost = 0;
+                                if( row.type == 'Pounds' ) { //changing pounds to kg
+                                    row.weight = row.weight / 2.2;
+                                }
+                                if( type !== "kg" ){ //changing pounds to kg
+                                    weight = weight / 2.2;                                
+                                }
+                                if( row.operation == 'Over' ){
+                                    if( weight >= row.weight  ){
+                                        //over = true;
+                                        //overCost = row.cost;
+                                        shippingRate = row.cost
+                                        throw BreakException
+                                    }
+                                }else{
+                                    if( weight < row.weight ){
+                                        //underCost = row.cost;
+                                        shippingRate = row.cost;
+                                        throw BreakException
+                                    }
+                                }
+                            });
+                        } catch (e) {
+                            console.log(country +' .. '+ weight +' .. '+ type);
+                            return shippingRate;
+                            if (e !== BreakException) throw e;
+                        }
+                        
+                    },
+                    error => {
+                        console.log(error);
+                    }
+                )
+                console.log( shippingRate );
+                return shippingRate;
+            }
+            catch (e) {
+                console.error(e);
+                res.serverError(e);
+            }
+    }
 };
 
