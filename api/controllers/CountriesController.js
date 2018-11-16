8b3895bc98cc081bfb5f8f1d9609df868bfc3441
Milton Espinoza <milton@senorcoders.com>
2018-11-16 14:11:47 -0600

    route for get all cities

--- api/controllers/CountriesController.js
+++ api/controllers/CountriesController.js
@@ -7,6 +7,29 @@
 
 module.exports = {
   
-
+    getAllCities: async (req, res)=>{
+        let countries = await Countries.find();
+        let cities = [];
+        let getCities = async () => {
+            await asyncForEach(countries, async (country) => {   
+                await asyncForEach(country.cities, async ( city ) => {
+                    cities.push( city );
+                } )
+            });
+            
+            console.log('Done');
+            
+            console.log( cities );
+            res.status(200).json( cities );
+          }
+        
+        getCities();
+        
+    }
 };
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
 
