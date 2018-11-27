18e10c03ce6389d756a3441232c27ab82d6175d3
Milton Espinoza <milton@senorcoders.com>
2018-11-27 18:15:30 +0000

    adding authorizatoin request for payfort

--- api/controllers/PaymentsController.js
+++ api/controllers/PaymentsController.js
@@ -18,7 +18,64 @@ module.exports = {
     let payment = await Payments.create( data ); 
     res.status(200).json( payment );
 
-  }
+  },
 
+  askForAuthorization: (req, res) => {
+    const axios = require('axios');
+
+    let params = req.params;
+    console.log( req.param('command') );
+    var mydata = {
+      "command": req.param('command'),
+      "access_code": req.param('access_code'),
+      "merchant_identifier": req.param('merchant_identifier'),
+      "merchant_reference": req.param('merchant_reference'),
+      "currency": req.param('currency'),
+      "language": req.param('language'),
+      "token_name": req.param('token_name'),
+      "signature": req.param('signature'),
+      "settlement_reference": req.param('settlement_reference'),
+      "customer_email": req.param('customer_email'),
+      "amount": req.param('amount'),
+      "order_description": req.param('order_description')
+   } ;
+
+
+let url	 = "https://sbpaymentservices.payfort.com/FortAPI/paymentApi";
+
+axios.post( url, mydata, { 'Content-Type': 'application/json', 'cache-control': 'no-cache' }  )
+  .then(function (response) {
+    console.log(response);
+    res.status(200).json(response.data)
+  })
+  .catch(function (error) {
+//    console.log(error);
+	if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+	res.status(200).send(error.data)
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+	res.status(200).send(error.request)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+      res.status(200).send(error);
+   }
+    console.log(error.config);
+//res.status(200).send(error)
+  });
+
+
+
+
+//  res.status(200).json( data );
+  }
 };
 
