b46b6685ededfa16dbee0d43a985bdc813c1d16e
Milton Espinoza <milton@senorcoders.com>
2019-01-08 15:21:50 -0600

    allow admin to change to any status

--- api/controllers/ItemShoppingController.js
+++ api/controllers/ItemShoppingController.js
@@ -269,7 +269,8 @@ module.exports = {
             }else if ( status == '5c13f453d827ce28632af048'){//pending fulfillment
                 let data=await ItemShopping.update({id}, { status: '5c13f453d827ce28632af048', cancelAt: ts, updateInfo: currentUpdateDates}).fetch();                
             }else{
-                res.status(400).send("status not found")
+                let data=await ItemShopping.update({id}, { status: status, updateInfo: currentUpdateDates}).fetch();                
+                res.status(200).json( { "message": "status updated" } );
             }
             res.status(200).json( { "message": "status updated" } );
 
