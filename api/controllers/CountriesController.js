2039a518f970d40407a40ae78ffce40f3afa563d
Milton Espinoza <milton@senorcoders.com>
2019-01-25 10:44:17 -0600

    adding city eta feature

--- api/controllers/CountriesController.js
+++ api/controllers/CountriesController.js
@@ -25,7 +25,29 @@ module.exports = {
         
         getCities();
         
+    },
+    
+    updateCityEta: async ( req, res ) => {
+        let countryID = req.body.country;
+        let cityCode = req.body.city;
+        let mineta = req.body.mineta;
+        console.log( 'id', countryID );
+        let country = await Countries.findOne( { id: countryID } );
+        console.log('country', country);
+        let cities = [];
+        await asyncForEach( country.cities, async ( city ) => {
+            if ( cityCode === city.code ){
+                city.mineta = mineta;
+            }
+            cities.push( city )
+        } )
+
+        let updated = await Countries.update( { id: countryID }, { cities: cities } );
+
+        res.status(200).json( updated );
+
     }
+
 };
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
