d3ab992b7ee21309ff21e4e70bd478e218e49cb2
Milton Espinoza <milton@senorcoders.com>
2019-03-13 16:41:00 -0600

    remove osama from dev

--- api/services/MailerService.js
+++ api/services/MailerService.js
@@ -1,7 +1,6 @@
 var nodeMailer = require("nodemailer");
 var Email = require('email-templates');
-// const ADMIN_EMAIL = 'osama@seafoodsouq.com, kharron@seafoodsouq.com, osmany@seafoodsouq.com';
-const ADMIN_EMAIL = 'osama@seafoodsouq.com, kharron@seafoodsouq.com';
+const ADMIN_EMAIL = 'kharron@seafoodsouq.com';
 const APP_NAME = sails.config.APP_NAME;
 const config = sails.config.mailer;
 const sender = config.auth.user;
