dbd204adbb0bbf6034ad8fbd0dc22c44ce60ee7f
Milton Espinoza <milton@seafoodsouq.com>
2019-03-22 09:01:29 -0600

    adding variation structure

---
+++ api/models/Variations.js
@@ -0,0 +1,44 @@
+/**
+ * Variations.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝    
+    sku: {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    fishPreparation: {
+      model: "fishPreparation",
+      required: true
+    },
+    fish: {
+      model: "fish",
+      required: true
+    },
+    wholeFishWeight: {
+      model: "wholeFishWeight",
+      required: false
+    }
+  },
+
+};
+
