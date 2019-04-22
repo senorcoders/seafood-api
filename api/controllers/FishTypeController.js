4425463188064f903eb9068e3e35e0175adfa2e2
Milton Espinoza <milton@seafoodsouq.com>
2019-04-22 10:31:37 -0600

    adding script for update category count

--- api/controllers/FishTypeController.js
+++ api/controllers/FishTypeController.js
@@ -242,40 +242,7 @@ module.exports = {
     },
     updateTypeCount: async (req, res) => {
         try {
-            let types = await FishType.find( { level: 0 } );
-
-            await Promise.all( types.map( async (type0) => {
-                // main category
-                let childs0 = await FishType.find( { level:1, parent: type0.id } );
-                let mainCount = 0;
-                await Promise.all(  childs0.map( async ( type1 ) => {
-
-                    // specie
-                    let specieCount = 0;
-                    let childs1 = await FishType.find( { level:2, parent: type1.id } );
-
-                    await Promise.all( childs1.map( async (type2) => {
-                        
-                        // subspecie
-                        let subSpecie = 0;
-                        let childs2 = await FishType.find( { level:3, parent: type2.id } );
-                        let fishes = await Fish.find( { type: type2.id, status: '5c0866f9a0eda00b94acbdc2' } );
-                        let fishCount = fishes.length;
-                        specieCount += fishCount;
-                        await FishType.update( { id: type2.id }, { totalFishes: fishCount } );
-
-
-                        type2.childs = childs2;
-                    } ) )
-                    await FishType.update( { id: type1.id }, { totalFishes: specieCount } );
-                    mainCount += specieCount;
-                    type1.childs = childs1;
-
-                } ) )
-                await FishType.update( { id: type0.id }, { totalFishes: mainCount } );
-                type0.childs = childs0;
-                
-            } ) )
+            await sails.helpers.updateCategoryCount();
             let utypes = await FishType.find();
             res.status(200).json( utypes );
         } catch (error) {
