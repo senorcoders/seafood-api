4972bae4c53c8ccd9c7b3cde73f746acc7e7ed80
Osmany Jirón <osmany@senorcoders.com>
2019-05-06 11:07:52 -0600

    para obtener los trimms para filleted, se tiene obtener los defaults primero

--- api/controllers/StoreTrimmingController.js
+++ api/controllers/StoreTrimmingController.js
@@ -7,31 +7,50 @@
 
 module.exports = {
 
-    getTrimmTypes : async (req, res) => {
+    getTrimmTypes: async (req, res) => {
         try {
             trimTypes = ["Trim A", "Trim B", 'Trim C', 'Trim D', 'Trim E'];
-            res.status( 200 ).json( trimTypes );
+            res.status(200).json(trimTypes);
         } catch (error) {
-            res.status( 400 ).json( error );
+            res.status(400).json(error);
         }
     },
-    getStoreTrimming : async ( req, res ) => {
+    getStoreTrimming: async (req, res) => {
         try {
             let storeID = req.param("store");
 
-            let storeTrim = await StoreTrimming.find( { store: storeID } ).populate('processingParts').sort( 'trimmingType ASC' );
+            // let storeTrim = await StoreTrimming.find( { store: storeID } ).populate('processingParts').sort( 'trimmingType ASC' );
 
+            // let result = [];
+            // await Promise.all( storeTrim.map( async ( item ) => {
+            //     let itemType = await TrimmingType.find( { id: item.trimmingType } );
+            //     item.type = itemType;
+
+            //     result.push( item );
+            // } ) )
+
+            let trimmingsDefault = await TrimmingType.find();
+            let storeTrim = await StoreTrimming.find({ store: storeID }).populate('processingParts').sort('trimmingType ASC');
             let result = [];
-            await await Promise.all( storeTrim.map( async ( item ) => {
-                let itemType = await TrimmingType.find( { id: item.trimmingType } );
-                item.type = itemType;
-
-                result.push( item );
-            } ) )
-
-            res.status( 200 ).json( result );
+            for (let trim of trimmingsDefault) {
+                let indexs = []; let ind = 0;
+                for (let str of storeTrim) {
+                    if (str.trimmingType === trim.id)
+                        indexs.push(ind);
+                    ind += 1;
+                }
+                if (indexs.length === 0)
+                    result.push({ type: [trim] });
+                else {
+                    for (let index of indexs) {
+                        storeTrim[index].type = [trim];
+                        result.push(storeTrim[index]);
+                    }
+                }
+            }
+            res.status(200).json(result);
         } catch (error) {
-            res.status( 400 ).json( error );
+            res.status(400).json(error);
         }
     }
 
