7c3f2387bdee51480ae19eb53f63e0981f8a5699
= <=>
2019-06-24 16:24:03 -0600

    no usamos el propmap en los items del carrito cuando se pagan porque hace los numeros en estring y se necesitan en operaciones matematicas

--- api/controllers/ShoppingCartController.js
+++ api/controllers/ShoppingCartController.js
@@ -792,7 +792,7 @@ module.exports = {
             await MailerService.sendCartPaidAdminNotified(itemsShopping, cart, OrderNumber, storeName)
 
             //Despues de generar el invoice se crea el correo
-            itemsShopping = await sails.helpers.propMap(itemsShopping, []);
+            // itemsShopping = await sails.helpers.propMap(itemsShopping, ['weight','boxWeight','price']);
             cart = await sails.helpers.propMap(cart, ["vat", "zipCode"]);
             if (cart.isCOD === true) {
                 await PDFService.buyerInvoiceCOD(itemsShopping, cart, OrderNumber, storeName, uaeTaxes[0].price);
