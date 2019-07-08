3c70b9a704ac16dc77716b6ba54b8701ceea3149
Milton Espinoza <milton@seafoodsouq.com>
2019-07-08 16:36:32 -0600

    fix price range when is out of stock

--- api/controllers/FishController.js
+++ api/controllers/FishController.js
@@ -945,8 +945,10 @@ module.exports = {
 
                 m['outOfStock'] = false;
                 if (minMaxInventory.length > 0) {
-                    m['max'] = Math.max.apply(null, minMaxInventory) // 4
-                    m.fish['maximumOrder'] = Math.max.apply(null, minMaxInventory);
+                    if(  Math.max.apply(null, minMaxInventory) > 0 ) {
+                        m['max'] = Math.max.apply(null, minMaxInventory) // 4
+                        m.fish['maximumOrder'] = Math.max.apply(null, minMaxInventory);
+                    }
                     m['outOfStock'] = false;
                     let dateParts = inventory[0].short_date.split('/');
                     m.fish['minInventoryDate'] = new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);//inventory[0].short_date;
@@ -1135,9 +1137,12 @@ module.exports = {
                     m['min'] = Math.min.apply(null, minMax) // 1
                 }
 
-                if (minMaxInventory.length > 0) {
-                    m['max'] = Math.max.apply(null, minMaxInventory) // 4
-                    m.fish['maximumOrder'] = Math.max.apply(null, minMaxInventory);
+                if (minMaxInventory.length > 0 ) {
+                    if(  Math.max.apply(null, minMaxInventory) > 0 ) {
+                        m['max'] = Math.max.apply(null, minMaxInventory) // 4
+                        m.fish['maximumOrder'] = Math.max.apply(null, minMaxInventory);
+                    }
+                    
                     m['outOfStock'] = false;
                     let dateParts = inventory[0].short_date.split('/');
                     m.fish['minInventoryDate'] = new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);//inventory[0].short_date;
@@ -1176,8 +1181,8 @@ module.exports = {
                     maxPrice.finalPrice = Number(parseFloat(maxPrice.finalPrice / fish.maximumOrder ).toFixed(2));//Math.max.apply(null, minMaxVariationPrices);
 
                 }
-                if (m.max === 0)
-                    maxPrice = minPrice;
+                //if (m.max === 0)
+                    //maxPrice = minPrice;
                     
                 if (minPrice.finalPrice > maxPrice.finalPrice) {
                     m['minPrice'] = maxPrice; // minPriceVar.min).toFixed(2));//Math.min.apply(null, minMaxVariationPrices);
