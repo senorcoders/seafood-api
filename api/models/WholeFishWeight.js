fd137af2ecb3fb1d60603f1d735542ceadfed6e7
Milton Espinoza <milton@seafoodsouq.com>
2019-07-15 17:03:10 -0600

    fixing min max fields

--- api/models/WholeFishWeight.js
+++ api/models/WholeFishWeight.js
@@ -18,11 +18,11 @@ module.exports = {
     },
     min: {
       type: 'number',
-      required: true
+      required: false
     },
     max: {
       type: 'number',
-      required: true
+      required: false
     }
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
