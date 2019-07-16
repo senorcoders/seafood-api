03ed94230718e451952dfeac3f628e7dd2b29bed
= <=>
2019-07-16 16:19:29 -0600

    cuando currency es AED cambiamos a uno, porque ahora se divide siempre los precios por exchange

--- api/helpers/calcs-prices.js
+++ api/helpers/calcs-prices.js
@@ -50,28 +50,17 @@ module.exports = {
           vat = 0, amount = 0;
         quantity = it.itemCharges.weight;
 
-        //if for seller email
-        switch (currency) {
-          case 'USD':
-            price = (Number(it.price) / exchangeRate).toFixed(2);
-            subtotal = (Number(it.subtotal) / exchangeRate).toFixed(2);;
-            taxesAndCustoms = (
-              (Number(it.itemCharges.customsFee) + Number(it.itemCharges.exchangeRateCommission) + Number(it.itemCharges.sfsMarginCost)) / exchangeRate
-            ).toFixed(2);
-            shippingHandles = (Number(it.itemCharges.shippingCost.cost) / exchangeRate).toFixed(2);
-            vat = (Number(it.itemCharges.uaeTaxesFee) / exchangeRate).toFixed(2);
-            amount = ((it.itemCharges.weight * it.itemCharges.price) / exchangeRate).toFixed(2);
-            break;
-          default:
-            price = it.price;
-            subtotal = it.subtotal;
-            taxesAndCustoms = (Number(it.itemCharges.customsFee) + Number(it.itemCharges.exchangeRateCommission) + Number(it.itemCharges.sfsMarginCost)).toFixed(2),
-              shippingHandles = it.itemCharges.shippingCost.cost;
-            vat = it.itemCharges.uaeTaxesFee;
-            amount = (it.itemCharges.weight * it.itemCharges.price).toFixed(2);
-            break;
+        //if currency is AED, is no necesary calc
+        if (currency === 'AED') currency = 1;
 
-        }
+        price = (Number(it.price) / exchangeRate).toFixed(2);
+        subtotal = (Number(it.subtotal) / exchangeRate).toFixed(2);;
+        taxesAndCustoms = (
+          (Number(it.itemCharges.customsFee) + Number(it.itemCharges.exchangeRateCommission) + Number(it.itemCharges.sfsMarginCost)) / exchangeRate
+        ).toFixed(2);
+        shippingHandles = (Number(it.itemCharges.shippingCost.cost) / exchangeRate).toFixed(2);
+        vat = (Number(it.itemCharges.uaeTaxesFee) / exchangeRate).toFixed(2);
+        amount = ((it.itemCharges.weight * it.itemCharges.price) / exchangeRate).toFixed(2);
 
         it.pricesCalc = {
           quantity,
