895683c70ba0283215eb19c6814a85252d643757
Milton Espinoza <milton@seafoodsouq.com>
2019-07-18 16:44:44 -0600

    create helper for update order status

--- api/helpers/get-eta-stock.js
+++ api/helpers/get-eta-stock.js
@@ -32,20 +32,26 @@ module.exports = {
   fn: async function (inputs, exits) {
     let variationID = inputs.variation; //variation id
     let quantity = inputs.quantity; // quantity to add to the cart
-   
-    let unixNow = Math.floor(new Date());
-    let stocks = await FishStock.find().where({
-        "date": { '>': unixNow },
-        "variations": variationID        
-    } ).sort( [{ date: 'ASC' }] );
+  
+    if( variationID && quantity ) {
+      let unixNow = Math.floor(new Date());
+      let stocks = await FishStock.find().where({
+          "date": { '>': unixNow },
+          "variations": variationID        
+      } ).sort( [{ date: 'ASC' }] );
 
-    //checking the query    
-    stocks.map( stock => {
-      if (stock.quantity - stock.purchased >= quantity) {
-        stock.available = stock.quantity - stock.purchased;
-        return exits.success( stock );
+      if ( stocks !== undefined ) {
+        //checking the query    
+        stocks.map( stock => {
+          if (stock.quantity - stock.purchased >= quantity) {
+            stock.available = stock.quantity - stock.purchased;
+            return exits.success( stock );
+          }
+        } )
       }
-    } )
+    }
+    
+    
 
 
     // Send back the result through the success exit.
