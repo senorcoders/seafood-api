6244808b014d4f75a1a7b0b9d1a5c2b77609ba73
Milton Espinoza <milton@seafoodsouq.com>
2019-08-02 17:24:56 -0600

    adding winston

--- config/log.js
+++ config/log.js
@@ -9,7 +9,62 @@
  * For more information on the Sails logger, check out:
  * https://sailsjs.com/docs/concepts/logging
  */
+let commonSettings = {
+  colorize: true,
+  maxsize: 10000000,
+  maxfiles: 10,
+  json: false,
+  
+};
 
+let settings = [
+  {filename: `${__dirname}/../logs/warn.log`, level: 'warn'},
+  {filename: `${__dirname}/../logs/error.log`, level: 'error'},
+  {filename: `${__dirname}/../logs/debug.log`, level: 'debug'},
+  {filename: `${__dirname}/../logs/info.log`, level: 'info', }
+];
+
+let {transports, createLogger, format} = require('winston');
+let loggerSettings = {
+  format: format.combine(
+    format.timestamp({format:'MM-YY-DD HH:mm:ss'}),
+    format.simple(),
+    //format.prettyPrint()
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`+(info.stack!==undefined?`${info.stack.substring(0,900)}...`:" "))
+),
+  transports: [...settings.map(
+    s => new transports.File({...s, ...commonSettings})), new transports.Console({
+    format: format.combine(
+      format.timestamp({format:'MM-YY-DD HH:mm:ss'}),
+      format.simple(),
+      //format.prettyPrint()
+      format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`+(info.splat!==undefined?`${info.splat}`:" "))
+  ),
+    //  format: format.simple()
+  })],
+  exitOnError: false
+};
+
+// This is the workaround
+let winstonLogger = createLogger(loggerSettings);
+let logger = {
+  'info': function () {
+      winstonLogger.info(...arguments);
+  },
+  'debug': function () {
+      winstonLogger.debug(...arguments);
+  },
+  'error': function () {
+      winstonLogger.error(...arguments);
+  },
+  'warn': function () {
+      winstonLogger.warn(...arguments);
+  },
+  'log': function () {
+      winstonLogger.log(...arguments);
+  }
+};
+// End of workaround
 module.exports.log = {
 
   /***************************************************************************
@@ -25,5 +80,6 @@ module.exports.log = {
   ***************************************************************************/
 
   // level: 'info'
-
+  custom: logger,
+  inspect: false
 };
